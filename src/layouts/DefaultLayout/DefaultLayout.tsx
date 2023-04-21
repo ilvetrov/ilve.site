69c6f8155317a4625ff299d3f9ca7d262512bd83
ilvetrov <ilya.ilvetrov@gmail.com>
2023-04-21 04:22:29 +0400

    feat: change animated color

--- src/layouts/DefaultLayout/DefaultLayout.tsx
+++ src/layouts/DefaultLayout/DefaultLayout.tsx
@@ -5,8 +5,8 @@ import { ILayout } from '~/interfaces/App.interface'
 import styles from './DefaultLayout.module.scss'
 
 const vars: string[] = ['--font-color-default-animated', '--font-color-link']
-const colors: string[] = ['#d5fc80']
-const colorsTimeouts = [5000, 3000]
+const colors: string[] = ['#fed105']
+const colorsTimeouts = [5000, 2000]
 
 const DefaultLayout: ILayout = ({ children }: { children: ReactNode }) => {
   return (
