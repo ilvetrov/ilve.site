40e3fe859c4ff44da33400b723fc10cac8d1b2ac
ilvetrov <ilya.ilvetrov@gmail.com>
2023-04-24 03:40:59 +0400

    chore: remove eslint rules for static and etc.

--- src/components/Modal/Center/Center.tsx
+++ src/components/Modal/Center/Center.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable jsx-a11y/no-static-element-interactions */
-/* eslint-disable jsx-a11y/click-events-have-key-events */
 import clsx from 'clsx'
 import { ReactNode, useRef } from 'react'
 import BottomCloseButton from '../BottomCloseButton/BottomCloseButton'
--- src/components/ShadowPageBackground/ShadowPageBackground.tsx
+++ src/components/ShadowPageBackground/ShadowPageBackground.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable jsx-a11y/no-static-element-interactions */
-/* eslint-disable jsx-a11y/click-events-have-key-events */
 import clsx from 'clsx'
 import { CSSProperties } from 'react'
 import styles from './ShadowPageBackground.module.scss'
